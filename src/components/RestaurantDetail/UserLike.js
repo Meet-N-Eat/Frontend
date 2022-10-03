e8ff21b6fa8e6f3d2773cb0637fb363cc8d36ca3
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-10-03 09:30:14 -0700

    Adds placeholder to UserLike for calls to SendFriendRequest and SendMessage components once built

--- src/components/RestaurantDetail/UserLike.js
+++ src/components/RestaurantDetail/UserLike.js
@@ -14,14 +14,22 @@ function UserLike({ user }) {
       else return false
    }
 
-   function clickHandler() {
-     friends() ? showMessage = true : showInvite = true
+   function clickHandler(e) {
+      // show appropriate modal based on friend status
+      if(e.target !== window) friends() ? showMessage = true : showInvite = true
+
+      // hide modal if user clicks outside of it
+      else {
+         showMessage = false
+         showInvite = false
+      }
    }
 
   return (
    <Col style={{ display: 'inline-block' }}>
       <img src={user.profileimg || 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT9JkaBnJ5fFI-FIGVM21jmBfS1HWlxWaAUDyaJQedJt2rc_RyW'} alt="" height="50px" width="50px" style={{borderRadius: '50%', aspectRatio: '1'}}/>
       <p>{user.username}</p>
+      {/* Add modals which call SendFriendRequest or SendMessage here */}
    </Col>
   )
 }
