69061bc10826c9d5f862ed4c06a81e1fca85a6fa
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-10-05 01:56:39 -0700

    builds SignUpShowHandler button click handler for non-logged in users

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import {Card, Button, Container, Image, Col, Row, ButtonGroup } from 'react-bootstrap/'
 import { Context } from '../App'
 import { axiosAll } from '../data-and-functions/axiosAll';
+import SignUp from './LoginSignUp/SignUp';
 
 
 const RestaurantCard = ({ restaurant, setLikeRefresh }) => {
@@ -10,6 +11,7 @@ const RestaurantCard = ({ restaurant, setLikeRefresh }) => {
     const notLikedImage = 'https://www.iconpacks.net/icons/1/free-heart-icon-492-thumb.png'
     const { colorTemplate, loggedInUser, dispatchUser }  = useContext(Context)
     const [buttonIcon, setButtonIcon] = useState(liked() ? likedImage : notLikedImage)
+    const [signUpShow, setSignUpShow] = useState(false)
     const { name, image_url, display_phone, price } = restaurant
     const { city, state } = restaurant.location
     const categories = []
@@ -33,63 +35,80 @@ const RestaurantCard = ({ restaurant, setLikeRefresh }) => {
             setButtonIcon(likedImage)
         }
     }
+
+    function signUpShowHandler() {
+        setSignUpShow(true)
+    }
     
     if (categories) {
         return (
-            <Card style={{marginBottom:'5%', fontSize:'70%', display:'flex', flexWrap:'wrap', justifyContent:'center', alignItems:'center', width:'100%', border:`1px solid ${colorTemplate.darkColor}`, margin:'1%', height:'100%'}} className="fluid">
-                <div style={{width:'100%'}}>
-                    <ButtonGroup style={{float:'right', margin:'1%'}}className="mb-2">
-                        <Button type="checkbox" variant="outline-light">
-                        <Image
-                        width={50}
-                        src={buttonIcon} 
-                        onClick={likeHandler}
-                        />
-                        </Button>
-                    </ButtonGroup>
-                </div>
-        <Card style={{border:'none', border:"none", padding:'5%', minWidth: '300px', minHeight: '400px'}} className="fluid">
-            <Link style={{ color:'black', textDecoration:'none' }} to={`/restaurants/${restaurant._id}`}>
-            <Card
-            style={{ display:'flex', flexDirection:'column'}}
-            className ="py-1 px-1 border-white ">
-                <Row style={{padding:'5%', borderRadius:'10px', backgroundColor:"white"}}>
-                    <Col >
-                        <Container style={{ textAlign:'center', display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center'}} className="ml-1">
-                            <Image 
-                                style={{ borderRadius:'10px', border:'1px solid #D6300F', marginBottom:'5%'}}
-                                src={image_url}
-                                alt="restaurant-image"
-                                width={170}
-                                height={170}
-                                />
-                            <Card.Title>{name}</Card.Title>
-                        </Container> 
-                    </Col>
-                    <Col>
-                        <Card.Body>
-                            <p>{price}</p>
-                            <p>M - F 9:00 AM - 8:00 PM</p>
-                            <Row>
-                                <Col>
-                                    <p>{city}, {state}</p>
-                                </Col>
-                                <Col>
-                                    <p>{display_phone}</p>
-                                </Col>
-                            </Row>
-                            {categories.map(category => 
+            <div>
+                <Card style={{marginBottom:'5%', fontSize:'70%', display:'flex', flexWrap:'wrap', justifyContent:'center', alignItems:'center', width:'100%', border:`1px solid ${colorTemplate.darkColor}`, margin:'1%', height:'100%'}} className="fluid">
+                    <div style={{width:'100%'}}>
+                        <ButtonGroup style={{float:'right', margin:'1%'}}className="mb-2">
+                            <Button type="checkbox" variant="outline-light">
+                            { loggedInUser.token ? 
+                            <Image
+                            width={50}
+                            src={buttonIcon} 
+                            onClick={likeHandler}
+                            />
+                            : 
+                            <Image
+                            width={50}
+                            src={buttonIcon} 
+                            onClick={signUpShowHandler}
+                            />
+                            }
+                            </Button>
+                        </ButtonGroup>
+                    </div>
+            <Card style={{border:'none', padding:'5%', minWidth: '300px', minHeight: '400px'}} className="fluid">
+                <Link style={{ color:'black', textDecoration:'none' }} to={`/restaurants/${restaurant._id}`}>
+                <Card
+                style={{ display:'flex', flexDirection:'column'}}
+                className ="py-1 px-1 border-white ">
+                    <Row style={{padding:'5%', borderRadius:'10px', backgroundColor:"white"}}>
+                        <Col >
+                            <Container style={{ textAlign:'center', display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center'}} className="ml-1">
+                                <Image 
+                                    style={{ borderRadius:'10px', border:'1px solid #D6300F', marginBottom:'5%'}}
+                                    src={image_url}
+                                    alt="restaurant-image"
+                                    width={170}
+                                    height={170}
+                                    />
+                                <Card.Title>{name}</Card.Title>
+                            </Container> 
+                        </Col>
+                        <Col>
+                            <Card.Body>
+                                <p>{price}</p>
+                                <p>M - F 9:00 AM - 8:00 PM</p>
                                 <Row>
-                                    {category}
+                                    <Col>
+                                        <p>{city}, {state}</p>
+                                    </Col>
+                                    <Col>
+                                        <p>{display_phone}</p>
+                                    </Col>
                                 </Row>
-                            )}
-                        </Card.Body>
-                    </Col>
-                </Row>
+                                {categories.map(category => 
+                                    <Row>
+                                        {category}
+                                    </Row>
+                                )}
+                            </Card.Body>
+                        </Col>
+                    </Row>
+                </Card>
+                </Link>
             </Card>
-            </Link>
-        </Card>
-        </Card>
+            </Card>
+            <div>
+                { signUpShow ? <SignUp /> : null }
+            </div>
+        </div>
     )
 }}
 
