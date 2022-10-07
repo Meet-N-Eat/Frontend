cfdc15f23b25e2919c95d82d47d0b10d205857f6
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-10-07 10:18:05 -0400

    adds fixed bottom navbar and styles carousel indicators in MyProfile.js

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -1,11 +1,15 @@
-import { useContext, useEffect, useReducer } from 'react'
+import { useContext, useEffect } from 'react'
 import LikedRestaurants from './LikedRestaurants'
 import Friends from './Friends'
 import CoordinateMeetup from '../CoordinateMeetup'
 import Itinerary from './Itinerary'
 import { axiosAll } from '../../data-and-functions/axiosAll'
 import { Context } from '../../App'
-import { Row, Col, Container, Carousel } from 'react-bootstrap'
+import { Navbar, Nav, Container, Carousel, Row, Col } from 'react-bootstrap'
+import './MyProfile.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUtensils, faUsers, faPeopleArrows, faCalendarDays } from '@fortawesome/free-solid-svg-icons'
+
 
 const MyProfile = () => {
 const { loggedInUser, dispatchUser } = useContext(Context)
@@ -20,22 +24,80 @@ if(!loggedInUser.response){
 
 return (
     <div>
-        <Carousel slide={false}>
-            <Carousel.Item className="carousel-item">
-                <CoordinateMeetup profile={loggedInUser.response}/>
-            </Carousel.Item>
-            <Carousel.Item className="carousel-item">
-                <Friends friends={loggedInUser.response.friends} />
-            </Carousel.Item>
-            <Carousel.Item className="carousel-item">
-                <LikedRestaurants likedrestaurants={loggedInUser.response.likedrestaurants} />
-            </Carousel.Item>
-            <Carousel.Item className="carousel-item">
-                <Itinerary profile={loggedInUser.response}/>
-            </Carousel.Item>
-        </Carousel>
-
+        <div>
+            <Navbar fixed='bottom' id='mypage-bar'>
+                    <Container>
+                        <Nav.Link id="mypage-tabs" data-bs-target aria-label="Slide 1">
+                            <Container id="mypage-tabs">
+                                <Row>
+                                    <FontAwesomeIcon icon={faPeopleArrows} />
+                                </Row>
+                                <Row>
+                                    invite
+                                </Row>
+                            </Container>
+                        </Nav.Link>
+                        <Nav.Link id="mypage-tabs" data-bs-target aria-label="Slide 2">
+                            <Container>
+                                <Row>
+                                    <FontAwesomeIcon icon={faUsers} />
+                                </Row>
+                                <Row>
+                                    friends
+                                </Row>
+                            </Container>
+                        </Nav.Link>
+                        <Nav.Link id="mypage-tabs" data-bs-target aria-label="Slide 3">
+                            <Container>
+                                <Row>
+                                    <FontAwesomeIcon icon={faUtensils} />
+                                </Row>
+                                <Row>
+                                    favorites
+                                </Row>
+                            </Container>
+                        </Nav.Link>
+                        <Nav.Link id="mypage-tabs" data-bs-target aria-label="Slide 4">
+                            <Container>
+                                <Row>
+                                    <FontAwesomeIcon icon={faCalendarDays} />
+                                </Row>
+                                <Row>
+                                    itinerary
+                                </Row>
+                            </Container>
+                        </Nav.Link>
+                    </Container>
+            </Navbar>
+        </div>
+        <div class="grid place-items-center h-screen">
+            <Carousel slide={false} wrap={false} prevIcon="dark">
+                <Carousel.Item>
+                    <div class="grid place-items-center h-screen">
+                        <CoordinateMeetup profile={loggedInUser.response}/>
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <div class="grid place-items-center h-screen">
+                        <Friends friends={loggedInUser.response.friends} />
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <div class="grid place-items-center h-screen">
+                        <LikedRestaurants likedrestaurants={loggedInUser.response.likedrestaurants} />
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <div class="grid place-items-center h-screen">
+                        <Itinerary profile={loggedInUser.response}/>
+                    </div>
+                </Carousel.Item>
+            </Carousel>
+        </div>
     </div>
+
+
+
     // <Container className='page-container'>
     //     <Row>
     //         {/* <Col>
