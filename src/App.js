7a1353ce9b6be32a2e9b3a7e375295861835c026
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-10-10 20:40:16 -0400

    updates Messages.js frontend route

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
             <Route path="/my-page" element={<MyPage />} />
             <Route path="/profile" element={<UserProfile />} />
             <Route path="/restaurants/:restaurantId" element={<RestaurantDetail />} />
-            <Route path="/message-center" element={<Messages />} />
+            <Route path="/messages" element={<Messages />} />
             <Route path="/messages/chat/:friendId" element={<MessageChat />} />
             <Route path="/faq" element={<FAQ />} />
             <Route path="/friendrequests" element={<FriendRequests />} />
