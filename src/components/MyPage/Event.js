4b274daca2b673570f6b1f29ddc18513a540f8a4
DavidJoao <davidsandoval596@gmail.com>
2022-10-16 10:07:01 -0700

    Cancel event function

--- src/components/MyPage/Event.js
+++ src/components/MyPage/Event.js
@@ -1,27 +1,43 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Card, OverlayTrigger, Popover, Button, ListGroup, Modal, NavDropdown } from 'react-bootstrap'
 import { BiDotsVertical } from 'react-icons/bi'
+import { Context } from '../../App'
+import { axiosAll } from '../../data-and-functions/axiosAll'
 
 const Event = ({ event }) => {
+    const { loggedInUser } = useContext(Context)
     const [modalState, setModalState] = useState(false)
     const [show, setShow] = useState(false);
+    const [showEdit, setShowEdit] = useState(false) 
  
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    const editClose = () => setShowEdit(false)
+    const editShow = () => setShowEdit(true)
+
     function toggleModal() {
         setModalState(!modalState)
     }
+
 console.log(event)
+
+    const handleCancel = () => {
+        axiosAll('DELETE', `/users/events/${event._id}`, loggedInUser.token)
+        handleClose()
+    }
+
 return (
 <Card style={{ width:'80%', marginBottom:'2%', border:"1px solid #D6300F" }}>
     <Card.Header style={{backgroundColor:'#D6300F', color:'white'}}>
         <div style={{display:'flex', justifyContent:'space-between'}}>
-            <p>date</p>
-            <p>hour</p>
-            <NavDropdown className="nav-dropdown d-inline-block" title={<BiDotsVertical style={{color:'white'}} size={20}/>}>
+            <p>{event.date}</p>
+            <NavDropdown className="nav-dropdown d-inline-block" title={<BiDotsVertical style={{color:'white'}} size={20} onClick={() => {console.log(event)}}/>}>
                     <NavDropdown.Item>
-                        <Button variant='danger' style={{color:'black'}}>Edit</Button>
+                        <Button variant='danger' style={{color:'black'}} onClick={editShow}>Edit</Button>
+                        <Modal show={showEdit} onHide={editClose}>
+                            {/* <CoordinateMeetup loggedInUser={loggedInUser} event={event} */}
+                        </Modal>
                     </NavDropdown.Item>
                     <NavDropdown.Item>
                         <Button variant="danger" onClick={handleShow} style={{color:'black'}}>Cancel Event</Button>
@@ -34,7 +50,7 @@ return (
                             <Button variant="secondary" onClick={handleClose} style={{color:'black'}}>
                                 Close
                             </Button>
-                            <Button variant="danger" onClick={handleClose} style={{backgroundColor:'#D6300F'}}>
+                            <Button variant="danger" onClick={handleCancel} style={{backgroundColor:'#D6300F'}}>
                                 Cancel Event
                             </Button>
                             </Modal.Footer>
