f5c11d71611cbd6344c30b1f57c14694c6eaba8d
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-10-27 16:58:12 -0700

    adds local state to Itinerary.js and call to events route
    Co-Authored-By: Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>

--- src/components/MyPage/Event.js
+++ src/components/MyPage/Event.js
@@ -1,14 +1,15 @@
 import moment from 'moment'
-import React, { useState, useEffect, useContext, useReducer } from 'react'
-import { Card, OverlayTrigger, Popover, Button, ListGroup, Modal, NavDropdown, Offcanvas } from 'react-bootstrap'
+import React, { useState, useEffect, useContext } from 'react'
+import { Card, OverlayTrigger, Popover, Button, ListGroup, Modal, NavDropdown } from 'react-bootstrap'
 import { BiDotsVertical } from 'react-icons/bi'
 import { Context } from '../../App'
-import { axiosAll, axiosReducer } from '../../data-and-functions/axiosAll'
+import { axiosAll } from '../../data-and-functions/axiosAll'
 import CoordinateMeetup from './CoordinateMeetup'
+import RestaurantCard from '../RestaurantCard'
 
-const Event = ({ event }) => {
+const Event = ({ event, updateEvents }) => {
     const { loggedInUser, dispatchUser } = useContext(Context)
-    const [restaurant, dispatchRestaurant] = useReducer(axiosReducer, null)
+    // const [restaurant, dispatchRestaurant] = useReducer(axiosReducer, null)
     const [modalState, setModalState] = useState(false)
     const [show, setShow] = useState(false);
     const [showEdit, setShowEdit] = useState(false) 
@@ -25,89 +26,77 @@ const Event = ({ event }) => {
     const formattedHour = (eventDate).format('hh:mm')
     const formattedHourAMPM = (eventDate).format('hh:mm A')
 
-
-    useEffect(() => {
-        if(restaurant) console.log(restaurant.response)
-    })
-
-    useEffect(() => {
-        axiosAll('GET', `/restaurants/${event.restaurant}`, loggedInUser.token, dispatchRestaurant)
-    },[])
-
     function toggleModal() {
         setModalState(!modalState)
     }
 
-    const handleCancel = () => {
-        axiosAll('DELETE', `/users/events/${event._id}`, loggedInUser.token, null)
+    const handleCancel = async () => {
+        await axiosAll('DELETE', `/users/events/${event._id}`, loggedInUser.token, null)
+        updateEvents()
         handleClose()
     }
-
-if(showEdit) {
-    return <CoordinateMeetup loggedInUser={loggedInUser} dispatchUser={dispatchUser} showEdit={showEdit} event={event} formattedDate={formattedDate} formattedHour={formattedHour} setShowEdit={setShowEdit} />
-}
-else {
-    return (
-        <Card style={{ width:'80%', marginBottom:'2%', border:"1px solid #D6300F" }}>
-            <Card.Header style={{backgroundColor:'#D6300F', color:'white'}}>
-                <div style={{display:'flex', justifyContent:'space-between'}}>
-                    <p>{formattedDate} at {formattedHourAMPM}</p>
-                    <NavDropdown className="nav-dropdown d-inline-block" title={<BiDotsVertical style={{color:'white'}} size={20} onClick={() => {console.log(event)}}/>}>
-                        <NavDropdown.Item>
-                            <Button variant='danger' style={{color:'black'}} onClick={() => {setShowEdit(true)}}>Edit</Button>
-                        </NavDropdown.Item>
-                        <NavDropdown.Item>
-                            <Button variant="danger" onClick={handleShow} style={{color:'black'}}>Cancel Event</Button>
-                            <Modal show={show} onHide={handleClose}>
-                                <Modal.Header closeButton>
-                                <Modal.Title>Confirm cancelation</Modal.Title>
-                                </Modal.Header>
-                                <Modal.Body>Are you sure you don't want to meet a creep and get free food?</Modal.Body>
-                                <Modal.Footer>
-                                <Button variant="secondary" onClick={handleClose} style={{color:'black'}}>
-                                        Close
-                                    </Button>
-                                    <Button variant="danger" onClick={handleCancel} style={{backgroundColor:'#D6300F'}}>
-                                        Cancel Event
-                                    </Button>
-                                    </Modal.Footer>
-                                </Modal>
+    console.log(event.participants)
+    if(showEdit) {
+        return <CoordinateMeetup loggedInUser={loggedInUser} dispatchUser={dispatchUser} showEdit={showEdit} event={event} formattedDate={formattedDate} formattedHour={formattedHour} setShowEdit={setShowEdit} />
+    }
+    else {
+        return (
+            <Card style={{ width:'80%', marginBottom:'2%', border:"1px solid #D6300F" }}>
+                <Card.Header style={{backgroundColor:'#D6300F', color:'white'}}>
+                    <div style={{display:'flex', justifyContent:'space-between'}}>
+                        <p>{formattedDate} at {formattedHourAMPM}</p>
+                        <NavDropdown className="nav-dropdown d-inline-block" title={<BiDotsVertical style={{color:'white'}} size={20} onClick={() => {console.log(event)}}/>}>
+                            <NavDropdown.Item>
+                                <Button variant='danger' style={{color:'black'}} onClick={() => {setShowEdit(true)}}>Edit</Button>
                             </NavDropdown.Item>
-                    </NavDropdown>
-                </div>
-            </Card.Header>
-            <Card.Body style={{ display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center' }}>
-    
-                <Card.Title>{restaurant && restaurant.response.name}</Card.Title>
-                <Card.Text>{restaurant && restaurant.response.display_phone}</Card.Text>
-                <Card.Text>{restaurant && restaurant.response.location.address1}</Card.Text>
-                <Card.Text>{restaurant && restaurant.response.location.city}</Card.Text>
-                {['bottom'].map((placement) => (
-                    <OverlayTrigger
-                    trigger="click"
-                    key={placement}
-                    placement={placement}
-                    overlay={
-                        <Popover id={`popover-positioned-${placement}`}>
-                        <Popover.Header style={{backgroundColor:'#F0704E', color:'white'}} as="h3">{`friends going`}</Popover.Header>
-                        <Popover.Body>
-                            <ListGroup>
-                                {event.participants.map(participant => <ListGroup.Item key={participant._id}><strong>{participant.displayname}</strong></ListGroup.Item>)}
-                            </ListGroup>
-                        </Popover.Body>
-                        </Popover>
-                    }
-                    >
-                    <Button 
-                        style={{backgroundColor:'white', border:'1px solid #D6300F', color:'black'}}
-                        onClick={toggleModal}
-                    >who's going?</Button>
-                    </OverlayTrigger>
-                ))}     
-            </Card.Body>
-        </Card>
-    )
-}
+                            <NavDropdown.Item>
+                                <Button variant="danger" onClick={handleShow} style={{color:'black'}}>Cancel Event</Button>
+                                <Modal show={show} onHide={handleClose}>
+                                    <Modal.Header closeButton>
+                                    <Modal.Title>Confirm cancelation</Modal.Title>
+                                    </Modal.Header>
+                                    <Modal.Body>Are you sure you don't want to meet a creep and get free food?</Modal.Body>
+                                    <Modal.Footer>
+                                    <Button variant="secondary" onClick={handleClose} style={{color:'black'}}>
+                                            Close
+                                        </Button>
+                                        <Button variant="danger" onClick={handleCancel} style={{backgroundColor:'#D6300F'}}>
+                                            Cancel Event
+                                        </Button>
+                                        </Modal.Footer>
+                                    </Modal>
+                                </NavDropdown.Item>
+                        </NavDropdown>
+                    </div>
+                </Card.Header>
+                <Card.Body style={{ display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center' }}>
+                    <RestaurantCard restaurant={event.restaurant} hideLikeButton={true} />
+                    {['bottom'].map((placement) => (
+                        <OverlayTrigger
+                        trigger="click"
+                        key={placement}
+                        placement={placement}
+                        overlay={
+                            <Popover id={`popover-positioned-${placement}`}>
+                            <Popover.Header style={{backgroundColor:'#F0704E', color:'white'}} as="h3">{`friends going`}</Popover.Header>
+                            <Popover.Body>
+                                <ListGroup>
+                                    {event.participants.map(participant => <ListGroup.Item key={participant._id}><strong>{participant.displayname || participant.username}</strong></ListGroup.Item>)}
+                                </ListGroup>
+                            </Popover.Body>
+                            </Popover>
+                        }
+                        >
+                        <Button 
+                            style={{backgroundColor:'white', border:'1px solid #D6300F', color:'black'}}
+                            onClick={toggleModal}
+                        >who's going?</Button>
+                        </OverlayTrigger>
+                    ))}     
+                </Card.Body>
+            </Card>
+        )
+    }
 }
 
 export default Event
--- src/components/MyPage/Itinerary.js
+++ src/components/MyPage/Itinerary.js
@@ -1,24 +1,33 @@
-import { useEffect } from 'react'
+import { useEffect, useReducer } from 'react'
+import { axiosAll, axiosReducer } from '../../data-and-functions/axiosAll'
 import Event from './Event'
 
-const Itinerary = ({ profile }) => {
-useEffect(() => console.log("Itinerary Rendered"))
-let today = new Date().toLocaleDateString()
+const Itinerary = ({ loggedInUser }) => {
+    const [events, dispatchEvents] = useReducer(axiosReducer, {})
+    useEffect(() => {
+        updateEvents()
+    }, [])
 
-return (
-    <div>
-            <div style={{ margin:'5%', display:'flex', justifyContent:'center', flexDirection:'column', alignItems:'center' }}>
-                <h2 style={{ borderBottom:'1px solid rgb(211,211,211)' }}>itinerary</h2>
-                <h2> {today} </h2>
-            </div>
-            <div style={{margin:'5%', height:'80%', display:'flex', flexDirection:'column', alignItems:'center' }}>
-                {profile.events.length > 0 ?
-                    profile.events.map(event => <Event event={event} key={event._id}/>)
-                    : <div>no events right now, send someone an invite!</div>
-                }
-            </div>
-    </div>
-)
+    let today = new Date().toLocaleDateString()
+
+    function updateEvents() {
+        axiosAll('GET', `/users/${loggedInUser.response._id}/events`, loggedInUser.token, dispatchEvents)
+    }
+
+    return (
+        <div>
+                <div style={{ margin:'5%', display:'flex', justifyContent:'center', flexDirection:'column', alignItems:'center' }}>
+                    <h2 style={{ borderBottom:'1px solid rgb(211,211,211)' }}>itinerary</h2>
+                    <h2> {today} </h2>
+                </div>
+                <div style={{margin:'5%', height:'80%', display:'flex', flexDirection:'column', alignItems:'center' }}>
+                    {events.response && events.response.length > 0 ?
+                        events.response.map(event => <Event event={event} updateEvents={updateEvents} key={event._id}/>)
+                        : <div>no events right now, send someone an invite!</div>
+                    }
+                </div>
+        </div>
+    )
 }
 
 export default Itinerary
