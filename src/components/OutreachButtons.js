9a48e58b1cda22127b077c9cdca2e4f08ccd2579
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-10-27 17:00:37 -0700

    adds error boundary and logic to hide buttons
    builds logic to hide outreach buttons for logged-in user
    
    Co-Authored-By: Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>

--- src/components/OutreachButtons.js
+++ src/components/OutreachButtons.js
@@ -1,8 +1,12 @@
+import { useContext } from 'react'
 import { Button } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
+import { Context } from '../App'
+
 
 function OutreachButtons( {friends, user, friendRequestHandler} ) {
   const navigate = useNavigate()
+  const { loggedInUser } = useContext(Context)
   
   const handleMessage = () => {
     navigate(`/messages/chat/${user.id}`)
@@ -10,10 +14,11 @@ function OutreachButtons( {friends, user, friendRequestHandler} ) {
 
   return (
     <div>
-      {friends && friends === true ?
-      <Button style={{color:'black'}} onClick={handleMessage}> Message {user.username}</Button>
-      :
-      <Button style={{color:'black'}} onClick={friendRequestHandler}>Add {user.username} as friend</Button> 
+      {user._id !== loggedInUser.response._id &&
+        (friends && friends === true ?
+        <Button style={{color:'black'}} onClick={handleMessage}> Message {user.displayname || user.username}</Button>
+        :
+        <Button style={{color:'black'}} onClick={friendRequestHandler}>Add {user.displayname || user.username} as friend</Button>) 
      }
     </div>
   )
