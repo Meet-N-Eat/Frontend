d70ca07834bdc83aa04252ec83d97e1d633f3879
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-10-30 03:15:28 -0700

    cleans up Home.js
    removes commented out/unused variables and legacy returns

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,61 +3,47 @@ import { Context } from '../../App'
 import { Row } from 'react-bootstrap'
 import Search from './Search'
 import SignUp from '../LoginSignUp/SignUp'
-// import CuisineCategory from './CuisineCategory'
 
 const Home = () => {
-// State hooks and Variables
-// ===========================================================================
-const { loggedInUser } = useContext(Context)
-const [signUpShow, setSignUpShow] = useState(false)
-// const categories = ['Italian', 'Southern', 'American (Traditional)']
+    // State hooks and Variables
+    // ===========================================================================
+    const { loggedInUser } = useContext(Context)
+    const [signUpShow, setSignUpShow] = useState(false)
 
-// Event handlers
-// ===========================================================================
-function signUpShowHandler() {
-    setSignUpShow(true)
-}
+    // Event handlers
+    // ===========================================================================
+    function signUpShowHandler() {
+        setSignUpShow(true)
+    }
 
-return (
-    
-    <div>
-        { loggedInUser.token ? 
-        <div id="loggedin-user-home">
-            <Row>
-                <Search />
-            </Row>
-        </div>
-        : 
+    return (
         <div>
-            <div id="new-user-home">
+            { loggedInUser.token ? 
+            <div id="loggedin-user-home">
                 <Row>
                     <Search />
                 </Row>
-                <Row>
-                    <button 
-                    id="signup-button"
-                    onClick={signUpShowHandler}
-                    >
-                        Create an account
-                    </button>
-                </Row>
             </div>
-            { signUpShow ? <SignUp /> : null }
+            : 
+            <div>
+                <div id="new-user-home">
+                    <Row>
+                        <Search />
+                    </Row>
+                    <Row>
+                        <button 
+                        id="signup-button"
+                        onClick={signUpShowHandler}
+                        >
+                            Create an account
+                        </button>
+                    </Row>
+                </div>
+                { signUpShow ? <SignUp /> : null }
+            </div>
+            }
         </div>
-        }
-    </div>
-    
-
-    // <Container style={{height:'90vh', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
-    //     <Row>
-    //         <Search />
-    //     </Row>
-    //     <h2 style={{paddingTop:'10%', marginBottom:'5%'}}>not sure where to go? start with a cuisine and see where it leads!</h2>
-    //     <Row className="d-flex">
-    //         {categories.map((category, index) => <CuisineCategory key={index} category={category} />)}
-    //     </Row>
-    // </Container>
-)
+    )
 }
 
 export default Home
