14c1713fd5faf8a1f6a52c65a71bd1900d995497
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-11-01 13:38:29 -0700

    updates layout of home and adds background color

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,40 +1,36 @@
-import { useContext, useState } from 'react'
-import { Context } from '../../App'
+import {useContext, useState} from 'react'
+import {Context} from '../../App'
 import Search from './Search'
 import SignUp from '../LoginSignUp/SignUp'
 
 const Home = () => {
-    // State hooks and Variables
-    // ===========================================================================
-    const { loggedInUser } = useContext(Context)
-    const [signUpShow, setSignUpShow] = useState(false)
+	// State hooks and Variables
+	// ===========================================================================
+	const {loggedInUser} = useContext(Context)
+	const [signUpShow, setSignUpShow] = useState(false)
 
-    // Event handlers
-    // ===========================================================================
-    function signUpShowHandler() {
-        setSignUpShow(true)
-    }
+	// Event handlers
+	// ===========================================================================
+	function signUpShowHandler() {
+		setSignUpShow(true)
+	}
 
-    return (
-        <div className='border border-black h-full'>
-            { loggedInUser.token ? 
-                    <Search />
-                : 
-                <>
-                    <Search />
-                    <div className='row-start-4'>
-                        <button 
-                            id="signup-button"
-                            onClick={signUpShowHandler}
-                        >
-                            Create an account
-                        </button>
-                    </div>
-                    { signUpShow ? <SignUp /> : null }
-                </>
-            }
-        </div>
-    )
+	return (
+		<div className='home h-36 w-10/12 mx-auto rounded-2xl flex flex-col justify-center space-y-2'>
+			<p className='text-white mx-auto'>Find restaurants you like? Meet others who like them too.</p>
+			<Search />
+			{!loggedInUser.token && (
+				<>
+					<button 
+						className='row-start-4 bg-white rounded-full p-1 mx-auto'
+						onClick={signUpShowHandler}>
+						Create an account
+					</button>
+					{signUpShow ? <SignUp /> : null}
+				</>
+			)}
+		</div>
+	)
 }
 
 export default Home
