18dc6f8c10fec8b35aa0018ec3ea09a52c25c20c
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-11-01 13:39:31 -0700

    updates layout and styling for Search

--- src/components/Home/Search.js
+++ src/components/Home/Search.js
@@ -1,71 +1,75 @@
-import { useReducer, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { getSearchParams } from "../../data-and-functions/searchParams"
-import { searchCriteriaReducer } from "../../data-and-functions/searchCriteriaReducer"
+import {useReducer, useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {getSearchParams} from '../../data-and-functions/searchParams'
+import {searchCriteriaReducer} from '../../data-and-functions/searchCriteriaReducer'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import SearchFilters from './SearchFilters'
 
 const Search = () => {
-    // Initial state for searchCriteria
-    // ===========================================================================
-    const initialState = {
-        searchString: '',
-        city: '',
-        category: '',
-        price: '',
-        wheelchairAccessible: '',
-        openLate: ''
-    }
+	// Initial state for searchCriteria
+	// ===========================================================================
+	const initialState = {
+		searchString: '',
+		city: '',
+		category: '',
+		price: '',
+		wheelchairAccessible: '',
+		openLate: '',
+	}
 
-    // State hooks and variable declarations
-    // ===========================================================================
-    const [searchCriteria, dispatch] = useReducer(searchCriteriaReducer, initialState)
-    const [showFilters, setShowFilters] = useState(false)
-    const navigate = useNavigate()
-    
-    // Event handlers
-    // ===========================================================================
-    function inputChange(e) {
-        dispatch({
-            key: 'searchString',
-            value: e.target.value})
-    }
+	// State hooks and variable declarations
+	// ===========================================================================
+	const [searchCriteria, dispatch] = useReducer(searchCriteriaReducer, initialState)
+	const [showFilters, setShowFilters] = useState(false)
+	const navigate = useNavigate()
 
-    function formSubmit(e) {
-        e.preventDefault()
-        const searchString = getSearchParams(searchCriteria)
-        searchCriteria && navigate(searchString)
-    }
-    
-    function filterClick() {
-        setShowFilters(prev => !prev)
-    }
+	// Event handlers
+	// ===========================================================================
+	function inputChange(e) {
+		dispatch({
+			key: 'searchString',
+			value: e.target.value,
+		})
+	}
 
-    return (
-        <div className="row-start-2 row-span-2">
-            <form onSubmit={formSubmit}>
-                <div>
-                    <div>
-                        <input
-                            type='text'
-                            placeholder='search by restaurant or category'
-                            onChange={inputChange}
-                            value={searchCriteria.searchString}
-                        />
-                    </div>
-                    <div>
-                        <button type='submit'>search</button>
-                        <button type='button' onClick={filterClick}>filters</button>
-                    </div>
-                </div>
-            </form>
-            <SearchFilters
-                searchCriteria={searchCriteria}
-                dispatch={dispatch} 
-                filterClick={filterClick}
-                showFilters={showFilters}
-            />
-        </div>
-    )
+	function formSubmit(e) {
+		e.preventDefault()
+		const searchString = getSearchParams(searchCriteria)
+		searchCriteria && navigate(searchString)
+	}
+
+	function filterClick() {
+		setShowFilters(prev => !prev)
+	}
+
+	return (
+		<div>
+			<form onSubmit={formSubmit}>
+				<div className='w-8/12 mx-auto grid grid-cols-5'>
+					<div className='col-span-4'>
+						<input
+							className='w-full p-1 rounded'
+							type='text'
+							placeholder='search by restaurant or category'
+							onChange={inputChange}
+							value={searchCriteria.searchString}
+						/>
+					</div>
+					<div>
+						<button type='button' onClick={filterClick}>
+							filters
+						</button>
+					</div>
+				</div>
+			</form>
+			<SearchFilters
+				searchCriteria={searchCriteria}
+				dispatch={dispatch}
+				filterClick={filterClick}
+				showFilters={showFilters}
+			/>
+		</div>
+	)
 }
 
 export default Search
