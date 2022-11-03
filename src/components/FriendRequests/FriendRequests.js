8a956b278fab1183b5ed5bb3cf7d20c33bf62344
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-11-04 02:08:15 +0900

    styles FriendRequest and FriendRequests components

--- src/components/FriendRequests/FriendRequests.js
+++ src/components/FriendRequests/FriendRequests.js
@@ -12,12 +12,14 @@ function FriendRequests() {
   },[])
 
   return (
-    <div>
-      {loggedInUser.token && 
-          friendRequests.response && friendRequests.response.length > 0 ? 
-            friendRequests.response.map(friendRequest => <FriendRequest key={friendRequest._id} friendRequest={friendRequest} dispatchRequests={dispatchRequests} />)
-            : <div>you don't have any invites right now</div>
-      }
+    <div className='flex items-center justify-center row-start-2'>
+      <div className='main-bg opacity-90 rounded-2xl px-6 md:px-14 py-4 md:py-5 space-y-4 w-80 md:w-[36rem] max-h-96 md:max-h-[80vh] overflow-y-auto'>
+        {loggedInUser.token && 
+            friendRequests.response && friendRequests.response.length > 0 ? 
+              friendRequests.response.map(friendRequest => <FriendRequest key={friendRequest._id} friendRequest={friendRequest} dispatchRequests={dispatchRequests} />)
+              : <div>you don't have any invites right now</div>
+        }
+      </div>
     </div>
   )
 }
