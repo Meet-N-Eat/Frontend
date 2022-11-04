9d4083d078926b7fd120e7d1aab2cd59b149f2bf
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-11-04 16:04:11 -0700

    adds styling for Pagination

--- src/components/MyPage/MyPage.js
+++ src/components/MyPage/MyPage.js
@@ -120,7 +120,7 @@ const MyPage = () => {
 					</div>
 				</div>
 			)}
-			<ul className='w-full h-12 flex flex-row justify-center items-center fixed left-0 bottom-0'>
+			<ul className='w-full h-12 flex justify-center items-center fixed left-0 bottom-0'>
 				{generateTabs()}
 			</ul>
 		</div>
