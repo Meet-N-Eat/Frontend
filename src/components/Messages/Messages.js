443fc0de4ef600b78fb0da3d136940e435aeb4c9
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-11-08 15:41:17 -0800

    updates tabs and formatting

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,67 +1,75 @@
-import { useEffect, useContext, useReducer } from 'react'
-import { Link } from 'react-router-dom'
-import { Dropdown } from 'react-bootstrap'
-import { Context } from '../../App'
-import { axiosAll, axiosReducer } from '../../data-and-functions/axiosAll'
-import { messageThreads } from '../../data-and-functions/messageThreads'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMessage } from '@fortawesome/free-solid-svg-icons'
+import {useEffect, useContext, useReducer} from 'react'
+import {Link} from 'react-router-dom'
+import {Dropdown} from 'react-bootstrap'
+import {Context} from '../../App'
+import {axiosAll, axiosReducer} from '../../data-and-functions/axiosAll'
+import {messageThreads} from '../../data-and-functions/messageThreads'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faMessage} from '@fortawesome/free-solid-svg-icons'
 import Message from './Message'
 import ProfileCard from '../ProfileCard'
 
 function Messages() {
-  const { loggedInUser } = useContext(Context)
-  const [messages, dispatchMessages] = useReducer(axiosReducer, {})
+	const {loggedInUser} = useContext(Context)
+	const [messages, dispatchMessages] = useReducer(axiosReducer, {})
 
-  useEffect(() => {
-    axiosAll('GET', `/users/${loggedInUser.response._id}/messages/all`, loggedInUser.token, dispatchMessages)
-  },[])
+	useEffect(() => {
+		axiosAll(
+			'GET',
+			`/users/${loggedInUser.response._id}/messages/all`,
+			loggedInUser.token,
+			dispatchMessages
+		)
+	}, [])
 
-  useEffect(() => {
-    messages.response && messageThreads(messages, loggedInUser)
-      .then(threadArray => {
-        dispatchMessages({
-          key: 'threadArray',
-          value: threadArray
-        })
-      })
-  }, [messages.response])
+	useEffect(() => {
+		messages.response &&
+			messageThreads(messages, loggedInUser).then(threadArray => {
+				dispatchMessages({
+					key: 'threadArray',
+					value: threadArray,
+				})
+			})
+	}, [messages.response])
 
-  return (
-    <div className='centered bg-red-900/90 w-3/4 mx-auto flex flex-col p-4 rounded-2xl'> 
-      <div className='w-full'>
-        <Dropdown className='float-right outline-none'>
-          <Dropdown.Toggle>
-            <FontAwesomeIcon className='' icon={faMessage}/>
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            {loggedInUser.response && 
-              loggedInUser.response.friends.map(friend => 
-                <Link 
-                  key={friend} 
-                  to={`/messages/${friend}`} 
-                >
-                  <ProfileCard user={friend} />
-                </Link>
-              )
-            }
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
-      {messages.threadArray && messages.threadArray.length > 0 ?
-        messages.threadArray.map(thread => 
-          <Link 
-            className='flex flex-col w-full p-1 rounded-xl text-black m-1'
-            key={thread[thread.length - 1]._id} 
-            to={`/messages/${thread[0].sender != loggedInUser.response._id ? thread[0].sender : thread[0].recipient}`} 
-          >
-            <Message message={thread[thread.length - 1]} />
-          </Link>
-        )
-        : <div>you don't have any messages, choose a friend from the list and start chatting!</div>
-      }
-    </div>
-  )
+	return (
+		<div className='centered main-bg w-3/4 mx-auto flex flex-col p-4 rounded-2xl'>
+			<div className='w-full'>
+				<Dropdown className='float-right outline-none'>
+					<Dropdown.Toggle className='border-none hover:bg-red-900 selection:bg-red-900 focus:bg-red-900 focus:ring-0'>
+						<FontAwesomeIcon className='' icon={faMessage} />
+					</Dropdown.Toggle>
+					<Dropdown.Menu>
+						{loggedInUser.response &&
+							loggedInUser.response.friends.map(friend => (
+								<Link key={friend} to={`/messages/${friend}`}>
+									<ProfileCard user={friend} />
+								</Link>
+							))}
+					</Dropdown.Menu>
+				</Dropdown>
+			</div>
+			{messages.threadArray && messages.threadArray.length > 0 ? (
+				messages.threadArray.map(thread => (
+					<Link
+						className='flex flex-col w-full p-1 rounded-xl text-black m-1'
+						key={thread[thread.length - 1]._id}
+						to={`/messages/${
+							thread[0].sender != loggedInUser.response._id
+								? thread[0].sender
+								: thread[0].recipient
+						}`}
+					>
+						<Message message={thread[thread.length - 1]} />
+					</Link>
+				))
+			) : (
+				<div>
+					you don't have any messages, choose a friend from the list and start chatting!
+				</div>
+			)}
+		</div>
+	)
 }
 
 export default Messages
--- src/components/MyPage/Itinerary.js
+++ src/components/MyPage/Itinerary.js
@@ -1,33 +1,41 @@
-import { useEffect, useReducer } from 'react'
-import { axiosAll, axiosReducer } from '../../data-and-functions/axiosAll'
+import {useEffect, useReducer} from 'react'
+import {axiosAll, axiosReducer} from '../../data-and-functions/axiosAll'
 import Event from './Event'
 
-const Itinerary = ({ loggedInUser }) => {
-    const [events, dispatchEvents] = useReducer(axiosReducer, {})
-    useEffect(() => {
-        updateEvents()
-    }, [])
+const Itinerary = ({loggedInUser}) => {
+	const [events, dispatchEvents] = useReducer(axiosReducer, {})
+	useEffect(() => {
+		updateEvents()
+	}, [])
 
-    let today = new Date().toLocaleDateString()
+	let today = new Date().toLocaleDateString()
 
-    function updateEvents() {
-        axiosAll('GET', `/users/${loggedInUser.response._id}/events`, loggedInUser.token, dispatchEvents)
-    }
+	function updateEvents() {
+		axiosAll(
+			'GET',
+			`/users/${loggedInUser.response._id}/events`,
+			loggedInUser.token,
+			dispatchEvents
+		)
+	}
 
-    return (
-        <div className='flex flex-col items-center w-full min-h-[830px]'>
-                <div>
-                    <h2 className='text-white'>itinerary</h2>
-                    <h2 className='text-white'> {today} </h2>
-                </div>
-                <div className='max-h-[720px] max-w-[335px] sm:max-w-[700px] gap-2 centered grid mx-auto grid-cols-1 lg:grid-cols-2 p-3 overflow-y-auto scroll'>
-                    {events.response && events.response.length > 0 ?
-                        events.response.map(event => <Event event={event} updateEvents={updateEvents} key={event._id}/>)
-                        : <div>no events right now, send someone an invite!</div>
-                    }
-                </div>
-        </div>
-    )
+	return (
+		<div className='flex flex-col items-center w-full min-h-[830px]'>
+			<div>
+				<h2 className='text-white'>itinerary</h2>
+				<h2 className='text-white'> {today} </h2>
+			</div>
+			<div className='max-h-[720px] max-w-[335px] sm:max-w-[700px] gap-2 centered grid mx-auto grid-cols-1 lg:grid-cols-2 p-3 overflow-y-auto scroll'>
+				{events.response && events.response.length > 0 ? (
+					events.response.map(event => (
+						<Event event={event} updateEvents={updateEvents} key={event._id} />
+					))
+				) : (
+					<div>no events right now, send someone an invite!</div>
+				)}
+			</div>
+		</div>
+	)
 }
 
 export default Itinerary
