b47db5b1b0caf9ff426421adf25ea90e331bca0c
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-11-10 22:31:11 +0900

    styles FriendRequestForm component

--- src/components/FriendRequests/FriendRequestForm.js
+++ src/components/FriendRequests/FriendRequestForm.js
@@ -1,51 +1,59 @@
-import React, {useContext, useReducer} from 'react'
-import {axiosAll, axiosReducer} from '../../data-and-functions/axiosAll'
-import {Context} from '../../App'
+import React, { useContext, useReducer } from 'react'
+import { axiosAll, axiosReducer } from '../../data-and-functions/axiosAll';
+import { Context } from '../../App'
+import { Spinner } from 'react-bootstrap'
 
-function FriendRequestForm({user, modalHandler}) {
-	// State Hooks and Variables
+function FriendRequestForm( {user, modalHandler} ) {
+  // State Hooks and Variables
 	// ===========================================================================
-	const {loggedInUser} = useContext(Context)
-	const initialState = {
-		sender: loggedInUser.response._id,
-		body: '',
-	}
-	const [request, dispatch] = useReducer(axiosReducer, initialState)
+  const { loggedInUser } = useContext(Context)
+  const initialState = {
+    sender: loggedInUser.response._id,
+    body: ''
+  }
+  const [request, dispatch] = useReducer(axiosReducer, initialState)
 
-	// Event Handlers and Functions
-	// ===========================================================================
-	const handleChange = e => {
-		dispatch({
-			key: 'body',
-			value: e.target.value,
-		})
-	}
+  // Event Handlers and Functions
+  // ===========================================================================
+  const handleChange = (e) => {
+    dispatch({
+      key: 'body',
+      value: e.target.value
+    })
+  }
 
-	const handleSubmit = () => {
-		axiosAll('POST', `/users/${user._id}/friendInvites`, loggedInUser.token, dispatch, request)
-		modalHandler()
-	}
+  const handleSubmit = () => {
+    axiosAll('POST', `/users/${user._id}/friendInvites`, loggedInUser.token, dispatch, request)
+    modalHandler()
+  }
 
 	// Return
 	// ===========================================================================
-	return (
-		<div className='modals'>
-			<div className='modals-content flex flex-col space-y-4'>
-				<p className='text-white text-center font-normal'>Message for {user.username}</p>
-				<input
-					className='input'
-					as='textarea'
-					placeholder='optional message'
-					onChange={handleChange}
-				></input>
-				<div className='flex justify-center'>
-					<button className='button' onClick={handleSubmit}>
-						Send
-					</button>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div
+    className='modals'
+    onClick={modalHandler}>
+      <div className='modals-content space-y-4'>
+        {user && user.username ? (
+        <>
+        <p className='header text-white text-xl text-center'>Add {user.username} as friend</p>
+        <input
+        className='input w-full'
+        as='textarea'
+        divs={3}
+        placeholder='Send an optional message'
+        maxlength='200'
+        onChange={handleChange}></input>
+        <p className='text-slate-300 text-sm'>maximum length: 200 characters</p>
+        <div className='centered'>
+          <button
+          className='button'
+          onClick={handleSubmit}>Add friend</button>
+        </div>
+        </>
+        ) : ( <Spinner animation='border' variant="light"/> )}
+      </div>
+    </div>
+  )
 }
-
 export default FriendRequestForm
