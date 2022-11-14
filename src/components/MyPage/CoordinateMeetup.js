5965d455f284a199a0833daa1f48803a46154dd6
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-11-14 13:07:47 -0800

    updates invite-container class to be responsive at all sizes

--- src/components/MyPage/CoordinateMeetup.js
+++ src/components/MyPage/CoordinateMeetup.js
@@ -157,7 +157,7 @@ const CoordinateMeetup = ({
 	// Return
 	// ===========================================================================
 	return (
-		<div className='main-bg vertical justify-evenly items-center h-full w-[335px] sm:invite-container'>
+		<div className='main-bg invite-container'>
 			<h1 className='white-header'>meet 'n eat with friends</h1>
 			{error.restaurant && <h2>select a restaurant for this event</h2>}
 			<select
