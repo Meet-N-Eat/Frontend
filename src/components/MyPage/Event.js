3d22ff6e9c19eeafb86fd6794b0719086dcf378d
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-11-17 16:32:36 -0800

    cleans up Event component

--- src/components/MyPage/Event.js
+++ src/components/MyPage/Event.js
@@ -1,13 +1,13 @@
 import moment from 'moment'
 import React, {useState, useEffect, useContext} from 'react'
-import { Modal, NavDropdown} from 'react-bootstrap'
+import {Modal, NavDropdown} from 'react-bootstrap'
 import {Context} from '../../App'
 import {axiosAll} from '../../data-and-functions/axiosAll'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faEllipsisVertical} from '@fortawesome/free-solid-svg-icons'
 import CoordinateMeetup from './CoordinateMeetup'
 import RestaurantCard from '../RestaurantCard'
 import ProfileCard from '../ProfileCard'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faEllipsisVertical} from '@fortawesome/free-solid-svg-icons'
 
 const Event = ({event, updateEvents}) => {
 	// State Hooks and Variables
@@ -17,18 +17,8 @@ const Event = ({event, updateEvents}) => {
 	const [showEdit, setShowEdit] = useState(false)
 	const [modalShow, setModalShow] = useState(false)
 
-
-	//cancel
-	const handleClose = () => setShow(false)
-	const handleShow = () => setShow(true)
 	const showHandler = () => {
 		setModalShow(!modalShow)
-	}
-
-	const closeEdit = () => setShowEdit(false)
-	const openEdit = () => setShowEdit(true)
-	const editHandler = () => {
-		setShowEdit(prevState => !prevState)
 	}
 
 	// DATE AND HOUR FORMAT
@@ -42,93 +32,111 @@ const Event = ({event, updateEvents}) => {
 	const handleCancel = async () => {
 		await axiosAll('DELETE', `/users/events/${event._id}`, loggedInUser.token, null)
 		updateEvents()
-		handleClose()
+		setShow(false)
 	}
-	
+
 	if (showEdit) {
 		return (
 			<CoordinateMeetup
 				loggedInUser={loggedInUser}
 				dispatchUser={dispatchUser}
-				showEdit={showEdit}
 				event={event}
 				formattedDate={formattedDate}
 				formattedHour={formattedHour}
+				showEdit={showEdit}
 				setShowEdit={setShowEdit}
 				updateEvents={updateEvents}
 			/>
 		)
 	} else {
-	
-	// Return
-	// ===========================================================================
-	return (
-		<div className='main-bg grid-centered p-2 w-[330px] h-[460px] mt-2'>
-			<div className='w-full'>
-				<div className='horizontal justify-around items-center w-full'>
-					<>
-						<p className='text-white base-text'>
-							{formattedDate} at {formattedHourAMPM}
-						</p>
-						<NavDropdown
-							className='nav-dropdown d-inline-block'
-							title={
-								<FontAwesomeIcon
-									icon={faEllipsisVertical}
-									className='icon text-white w-1/2 float-right'
-								/>
-							}
-						>
-							<NavDropdown.Item>
-								<button className='base-text w-full' onClick={openEdit}>Edit</button>
-							</NavDropdown.Item>
-							<NavDropdown.Item>
-								<button className='base-text w-full' onClick={handleShow}>Cancel Event</button>
-								<Modal size='xl' show={show} onHide={handleClose}>
-									<div className='modals-content h-[200px] flex flex-col justify-around p-3'>
-										<div closeButton className='text-center'>
-											<h1 className='base-text'>Confirm cancelation</h1>
+		// Return
+		// ===========================================================================
+		return (
+			<div className='main-bg grid-centered p-2 w-[330px] h-[460px] mt-2'>
+				<div className='w-full'>
+					<div className='horizontal justify-around items-center w-full'>
+						<>
+							<p className='text-white base-text'>
+								{formattedDate} at {formattedHourAMPM}
+							</p>
+							<NavDropdown
+								className='nav-dropdown d-inline-block'
+								title={
+									<FontAwesomeIcon
+										icon={faEllipsisVertical}
+										className='icon text-white w-1/2 float-right'
+									/>
+								}
+							>
+								<NavDropdown.Item>
+									<button
+										className='base-text w-full'
+										onClick={() => setShowEdit(true)}
+									>
+										Edit
+									</button>
+								</NavDropdown.Item>
+								<NavDropdown.Item>
+									<button className='base-text w-full' onClick={() => setShow(true)}>
+										Cancel Event
+									</button>
+									<Modal size='xl' show={show} onHide={() => setShow(false)}>
+										<div className='modals-content h-[200px] flex flex-col justify-around p-3'>
+											<div closeButton className='text-center'>
+												<h1 className='base-text'>Confirm cancelation</h1>
+											</div>
+											<div className='text-xs md:text-sm text-center'>
+												Are you sure you want to cancel this event?
+											</div>
+											<div className='flex flex-row justify-between'>
+												<button
+													className='base-text button mr-1'
+													onClick={() => setShow(false)}
+												>
+													Close{' '}
+												</button>
+												<button
+													className='base-text account-button ml-1'
+													onClick={handleCancel}
+												>
+													Cancel event{' '}
+												</button>
+											</div>
 										</div>
-										<div className='text-xs md:text-sm text-center'>
-											Are you sure you want to cancel this event?
-										</div>
-										<div className='flex flex-row justify-between'>
-											<button className='base-text button mr-1' onClick={handleClose}>Close </button>
-											<button className='base-text account-button ml-1' onClick={handleCancel}>Cancel event </button>
-										</div>
-									</div>
-								</Modal>
-							</NavDropdown.Item>
-						</NavDropdown>
-					</>
-				</div>
-			</div>
-			<div className='grid-centered'>
-				<RestaurantCard restaurant={event.restaurant} hideLikeButton={true} />
-				<Modal
-				show={modalShow}
-				onHide={showHandler}
-				aria-labelledby="whosgoing-modal"
-				>
-					<div className='modals-content'>
-						<Modal.Header>
-							<Modal.Title className='text-white mx-auto'>who's going?</Modal.Title>
-						</Modal.Header>
-						<Modal.Body className=''>
-							<div className='grid-centered overflow-auto'>
-								{event.participants.map(participant => (
-									<ProfileCard modalShow={modalShow} key={participant} user={participant} />
-									))}
-							</div>
-						</Modal.Body>
+									</Modal>
+								</NavDropdown.Item>
+							</NavDropdown>
+						</>
 					</div>
-				</Modal>
-				<div className='py-2'>
-					<button className='button base-text' onClick={showHandler}>Who's going?</button>
+				</div>
+				<div className='grid-centered'>
+					<RestaurantCard restaurant={event.restaurant} hideLikeButton={true} />
+					<Modal show={modalShow} onHide={() => setModalShow(true)} aria-labelledby='whosgoing-modal'>
+						<div className='modals-content'>
+							<Modal.Header>
+								<Modal.Title className='text-white mx-auto'>who's going?</Modal.Title>
+							</Modal.Header>
+							<Modal.Body className=''>
+								<div className='grid-centered overflow-auto'>
+									{event.participants.map(participant => (
+										<ProfileCard
+											modalShow={modalShow}
+											key={participant}
+											user={participant}
+										/>
+									))}
+								</div>
+							</Modal.Body>
+						</div>
+					</Modal>
+					<div className='py-2'>
+						<button className='button base-text' onClick={() => setModalShow(false)}>
+							Who's going?
+						</button>
+					</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
 	}
 }
 
