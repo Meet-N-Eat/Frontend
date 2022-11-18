89d4ac8e4bf3a99e4b8c6163bcf0da41dc68bc1d
DavidJoao <davidsandoval596@gmail.com>
2022-11-17 21:17:56 -0800

    Add stars rating to reviews

--- src/components/RestaurantDetail/Review.js
+++ src/components/RestaurantDetail/Review.js
@@ -24,8 +24,11 @@ const Review = ({ review }) => {
             {reviewer.response ? 
                 <ul className='horizontal justify-between items-center main-bg m-1 border'>
                     <li className='min-h-[70px] max-w-[90px] p-2 text-white base-text rounded-tl-2xl rounded-bl-2xl '>{reviewer.response.displayname || reviewer.response.username}</li>
-                    <li className='w-full min-h-[70px] grid-centered text-white base-text'>{review.body}</li>
-                    <li className='min-h-[70px] float-right max-w-[100px] grid-centered text-white base-text rounded-br-2xl rounded-tr-2xl'>{`${date} ${time}`}</li>
+                    <li className='w-3/4 min-h-[70px] flex items-center text-white base-text'>{review.body}</li>
+                    <div className='p-1'>
+                        <li>{'☆'.repeat(review.stars)}</li>
+                        <li className='min-h-[70px] float-right max-w-[100px] grid-centered text-white base-text rounded-br-2xl rounded-tr-2xl'>{`${date} ${time}`}</li>
+                    </div>
                 </ul>
                 : 
                 <div className='text-center p-4'>
