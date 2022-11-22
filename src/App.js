960e7c8c5a640cbb669253891d3d2aeda49f765a
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-11-22 15:37:51 -0800

    adds PersistentLogin component

--- src/App.js
+++ src/App.js
@@ -1,53 +1,75 @@
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route} from 'react-router-dom'
 import useAuth from './hooks/useAuth'
-import About from "./components/About"
-import Home from "./components/Home/Home"
-import Messages from "./components/Messages/Messages"
-import MessageChat from "./components/Messages/MessageChat"
-import MyPage from "./components/MyPage/MyPage"
-import NavBar from "./components/Navigation/NavBar"
-import RestaurantDetail from "./components/RestaurantDetail/RestaurantDetail"
-import SearchResults from "./components/SearchResults/SearchResults"
-import UserProfile from "./components/UserProfile"
-import FriendRequests from "./components/FriendRequests/FriendRequests"
-import LogIn from "./components/LoginSignUp/LogIn"
-import SignUp from "./components/LoginSignUp/SignUp"
-import CoordinateMeetup from "./components/MyPage/CoordinateMeetup"
-import Friends from "./components/MyPage/Friends"
-import Favorites from "./components/MyPage/Favorites"
-import Itinerary from "./components/MyPage/Itinerary"
+import Layout from './components/Layout'
+import PersistentLogin from './components/PersistentLogin'
+import About from './components/About'
+import Home from './components/Home/Home'
+import Messages from './components/Messages/Messages'
+import MessageChat from './components/Messages/MessageChat'
+import MyPage from './components/MyPage/MyPage'
+import NavBar from './components/Navigation/NavBar'
+import RestaurantDetail from './components/RestaurantDetail/RestaurantDetail'
+import SearchResults from './components/SearchResults/SearchResults'
+import UserProfile from './components/UserProfile'
+import FriendRequests from './components/FriendRequests/FriendRequests'
+import LogIn from './components/LoginSignUp/LogIn'
+import SignUp from './components/LoginSignUp/SignUp'
+import CoordinateMeetup from './components/MyPage/CoordinateMeetup'
+import Friends from './components/MyPage/Friends'
+import Favorites from './components/MyPage/Favorites'
+import Itinerary from './components/MyPage/Itinerary'
 
 function App() {
-   const {loggedInUser, dispatchUser} = useAuth()
+	const {loggedInUser, dispatchUser} = useAuth()
 
-   return (
-      <div className='App h-full'>
-         <header className="row-start-1 row-span-1">
-            <NavBar />
-         </header>
-         <main>
-            <Routes>
-               <Route path='/' element={<Home />} />
-               <Route path='/users/authentication/login' element={<LogIn />} />
-               <Route path='/users/authentication/signup' element={<SignUp />} />
-               <Route path='/results/:searchString' element={<SearchResults />} />
-               <Route path='/my-page' element={<MyPage />} >
-                  <Route index element={<CoordinateMeetup loggedInUser={loggedInUser} dispatchUser={dispatchUser} />} />
-                  <Route path='invite' element={<CoordinateMeetup loggedInUser={loggedInUser} dispatchUser={dispatchUser} />} />
-                  <Route path='friends' element={<Friends loggedInUser={loggedInUser} />} />
-                  <Route path='favorites' element={<Favorites loggedInUser={loggedInUser} />} />
-                  <Route path='itinerary' element={<Itinerary loggedInUser={loggedInUser} />} />
-               </Route>
-               <Route path='/profile' element={<UserProfile />} />
-               <Route path='/restaurants/:restaurantId' element={<RestaurantDetail />} />
-               <Route path='/messages' element={<Messages />} />
-               <Route path='/messages/:friendId' element={<MessageChat />} />
-               <Route path='/about' element={<About />} />
-               <Route path='/friendrequests' element={<FriendRequests />} />
-            </Routes>
-         </main>
-      </div>
-   )
+	return (
+		<Routes>
+			<Route element={<PersistentLogin />}>
+				<Route path='/' element={<Layout />}>
+					<Route path='/' element={<Home />} />
+					<Route path='users/authentication/login' element={<LogIn />} />
+					<Route path='users/authentication/signup' element={<SignUp />} />
+					<Route path='results/:searchString' element={<SearchResults />} />
+					<Route path='restaurants/:restaurantId' element={<RestaurantDetail />} />
+					<Route path='about' element={<About />} />
+
+					<Route path='my-page' element={<MyPage />}>
+						<Route
+							index
+							element={
+								<CoordinateMeetup
+									loggedInUser={loggedInUser}
+									dispatchUser={dispatchUser}
+								/>
+							}
+						/>
+						<Route
+							path='invite'
+							element={
+								<CoordinateMeetup
+									loggedInUser={loggedInUser}
+									dispatchUser={dispatchUser}
+								/>
+							}
+						/>
+						<Route path='friends' element={<Friends loggedInUser={loggedInUser} />} />
+						<Route
+							path='favorites'
+							element={<Favorites loggedInUser={loggedInUser} />}
+						/>
+						<Route
+							path='itinerary'
+							element={<Itinerary loggedInUser={loggedInUser} />}
+						/>
+					</Route>
+					<Route path='profile' element={<UserProfile />} />
+					<Route path='messages' element={<Messages />} />
+					<Route path='messages/:friendId' element={<MessageChat />} />
+					<Route path='friendrequests' element={<FriendRequests />} />
+				</Route>
+			</Route>
+		</Routes>
+	)
 }
 
 export default App
